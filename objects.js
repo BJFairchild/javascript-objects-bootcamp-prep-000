516c2c0186ac35d5622a872247e5ac5c99aca9ad
Brittan Fairchild <brittanfairchild@gmail.com>
2019-06-12 22:00:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,3 +8,7 @@ function updatePlaylist(playlist, name, title) {
   playlist[name] = title ;
   return playlist ;
 }
+function removeFromPlaylist(playlist, name) {
+  delete playlist.name;
+  return playlist;
+}
