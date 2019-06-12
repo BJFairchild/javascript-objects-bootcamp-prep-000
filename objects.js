d83d1b48b827eeeb22dc2593227ed0b331a01531
Brittan Fairchild <brittanfairchild@gmail.com>
2019-06-12 21:57:30 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  artistbob: "Bob Song",
+  artistjoe: "Joe Song",
+  artistsam: "Sam Song"
+};
+
+function updatePlaylist(playlist, name, title) {
+  playlist[name] = title ;
+  return playlist ;
+}
